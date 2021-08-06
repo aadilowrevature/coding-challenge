259a97b9651614466f10c8adddb667d8cf041b73
aadilowrevature <abeil.adilow@revature.net>
2021-08-06 15:12:32 -0400

    update

--- 8-2-2021/challenge/src/main/java/com/challenge/main/challenge1.java
+++ 8-2-2021/challenge/src/main/java/com/challenge/main/challenge1.java
@@ -2,47 +2,104 @@ package com.challenge.main;
 
 public class challenge1 {
     public static void main(String[] args) {
-        String s1="abcdef";
-        String s2="verify";
+        String s1 = "abcdef";
+        String s2 = "verify";
 
-        String s1b="foggy";
-        String s2b="ydog";
-        String ans=solution(s1b,s2b);
+        String s1a="Minneapolis";
+        String s2a="Minnesota";
 
-        System.out.println(ans);
+        String s1b = "foggy";
+        String s2b = "ydog";
+
+        String s1c="god";
+        String s2c="good";
+
+
+        solution(s1, s2);
+        solution(s1a, s2a);
+        solution(s1b, s2b);
+        solution(s1c, s2c);
 
     }
 
-    public static String solution(String input1,String input2){
-        String ans="";
-        String check1="";
-        String check2="";
-        for(int i = 0; i <input2.length(); i ++){
-            if(input1.contains(""+input2.charAt(i))){
+    public static String solution(String input1, String input2) {
+        String ans = "";
+        String check1 = "";
+        String check2 = "";
+        for (int i = 0; i < input2.length(); i++) {
+            if (input1.contains("" + input2.charAt(i))) {
 
-                check1+=input2.charAt(i);
+                check1 += input2.charAt(i);
             }
         }
 
-        for(int i = 0; i <input1.length(); i ++){
-            if(input2.contains(""+input1.charAt(i))){
+        for (int i = 0; i < input1.length(); i++) {
+            if (input2.contains("" + input1.charAt(i))) {
 
-                check2+=input1.charAt(i);
+                check2 += input1.charAt(i);
             }
         }
 
-        System.out.println(check1+" "+check2);
-        if(check1.equals(check2)){
-            ans=check1;
-        }
-        else{
-            ans="";
-            for(int i=0;i<check1.length();i++){
-                if(check1.charAt(i)==check2.charAt(i)){
-                    ans+=check1.charAt(i);
+        System.out.println(check1 + " " + check2);
+        if (check1.equals(check2)) {
+            ans = check1;
+        } else {
+            ans = "";
+            int L1 = check1.length();
+            int L2 = check2.length();
+
+            String[] temp = new String[2 * L1];
+            String[] temp2 = new String[2 * L2];
+
+            for (int i = 0; i < L1; i++) {
+                temp[i] = "";
+                temp[i + L1] = "";
+                for (int j = i; j < L1; j++) {
+                    temp[i] += "" + check1.charAt(j);
+                }
+                for (int k = 0; k < i; k++) {
+                    temp[i + L1] += "" + check1.charAt(k);
+                }
+            }
+
+            for (int i = 0; i < L2; i++) {
+                temp2[i] = "";
+                temp2[i + L2] = "";
+                for (int j = i; j < L2; j++) {
+                    temp2[i] += "" + check2.charAt(j);
+                }
+                for (int k = 0; k < i; k++) {
+                    temp2[i + L2] += "" + check2.charAt(k);
+                }
+            }
+
+            for (int i = 0; i < temp.length; i++) {
+                for (int j = 0; j < temp2.length; j++) {
+                    if (temp[i].equals(temp2[j])) {
+
+                        if(temp[i].length()>ans.length()){
+                            ans=temp[i];
+                        }
+                    }
+                }
+            }
+
+            if(L1<L2) {
+                for (int i = 0; i < L1; i++) {
+                    for (int j = 0; j < L2; j++) {
+                        if(check1.charAt(i)==check2.charAt(j))
+                    }
+                }
+            } else {
+                for (int i = 0; i < L2; i++) {
+                    for (int j = 0; j < L1; j++) {
+
+                    }
                 }
             }
         }
+
+        System.out.println("LCS: " + ans);
         return ans;
     }
 }
--- 8-2-2021/logical-reasoning.md
+++ 8-2-2021/logical-reasoning.md
@@ -12,25 +12,25 @@ If a number is not prime it may have multiple factors, meaning it is possible to
 
 1<=(A-Z)*(1-26)<=26
 
-P=D4=4*4=16
-R=F3=6*3=18
-O=C5=3*5=15
-C=C1=C*1=3
-E=E1=5*1=5
-S=S1=19*1=19
-S=S1=19*1=19
-O=E3=5*3=15
-R=C6=3*6=18
+P=D4=4*4=16\
+R=F3=6*3=18\
+O=C5=3*5=15\
+C=C1=C*1=3\
+E=E1=5*1=5\
+S=S1=19*1=19\
+S=S1=19*1=19\
+O=E3=5*3=15\
+R=C6=3*6=18\
 
 ## Encode Quadrant
 
-Q=17=Q1
-U=21=U1
-A=1=A1
-D=4=D1
-R=18=R1
-A=1=A1
-N=14=N1
-T=20=T1
+Q=17=Q1\
+U=21=U1\
+A=1=A1\
+D=4=D1\
+R=18=R1\
+A=1=A1\
+N=14=N1\
+T=20=T1\
 
 Q1U1A1D1R1A1N1T1
